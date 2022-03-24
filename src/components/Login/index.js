6f1e4b2ab23ede15e9655a5be43b725bade54ce9
= <=>
2022-03-23 19:38:52 -0600

    Login page (no backend logic)

--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,7 +1,38 @@
+import { useState } from "react";
+
+import "./index.scss";
+
 export default function Login() {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleSubmit(e) {
+    e.preventDefault();
+    // Logic to login the user goes here
+  }
+
   return (
     <div className="login">
-      <h1>Login</h1>
+      <div className="login-form">
+        <h1 className="form-title">Login</h1>
+        <form action="" onSubmit={handleSubmit}>
+          <input
+            type="email"
+            placeholder="Write email here"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <input
+            type="password"
+            placeholder="Write password here"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="submit" className="login-btn">
+            Log in
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
