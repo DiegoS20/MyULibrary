6993cf5fb630ce5b81bf88799c37f3c041ac0dcc
= <=>
2022-03-24 17:07:50 -0600

    Adding route to the student dashboard

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import { Routes, Route } from "react-router-dom";
 import Login from "./components/Login";
+import Student from "./components/Student";
 
 function App() {
   return (
     <main>
       <Routes>
-        <Route path="/login" element={<Login />} />
+        <Route path="login" element={<Login />} />
+        <Route path="student" element={<Student />} />
       </Routes>
     </main>
   );
